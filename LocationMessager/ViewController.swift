12b0a8f99506a6b0a2ab7d0a905bc5dc4678d30f
Jay Shah <jeyshah9@gmail.com>
2018-04-08 01:38:50 -0700

    Adding App Icon

--- LocationMessager/ViewController.swift
+++ LocationMessager/ViewController.swift
@@ -7,8 +7,9 @@
 //
 
 import UIKit
+import MessageUI
 
-class ViewController: UIViewController {
+class ViewController: UIViewController , MFMessageComposeViewControllerDelegate{
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +21,39 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func SendBtnClicked(_ sender: UIButton) {
+        let composeVC = MFMessageComposeViewController()
+        composeVC.messageComposeDelegate = self
+        
+        // Configure the fields of the interface.
+        composeVC.recipients = ["XXXXXXXX"]
+        composeVC.body = "Reached";
+        
+        if !MFMessageComposeViewController.canSendText() {
+            print("SMS services are not available")
+        }
+        
+        if(sender.tag==0){
+            print("Reached");
+            composeVC.body = "Reached 😘 .. Have a nice day 😘";
+        }else if (sender.tag==1){
+            print("leaving");
+            composeVC.body = "Leaving now 😉 ... See you soon";
+        }
+        
+        // Present the view controller modally.
+        self.present(composeVC, animated: true, completion: nil)
+        
+    }
+    
+    func messageComposeViewController(_ controller: MFMessageComposeViewController,
+                                      didFinishWith result: MessageComposeResult) {
+        
+        controller.dismiss(animated: true, completion: nil)
+        
+    }
+    
+   
+    
 }
 
