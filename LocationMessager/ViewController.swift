a0121202cbe1eb724ac97007a88982f7cb44f61b
Jay Shah <jeyshah9@gmail.com>
2018-04-07 22:56:29 -0700

    Initial Commit

---
+++ LocationMessager/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LocationMessager
+//
+//  Created by Jay Shah on 4/7/18.
+//  Copyright © 2018 Jay Shah. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
